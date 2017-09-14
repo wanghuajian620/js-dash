6ffbf9ed5fc932b45ec97ecbf31d75373c5215d2
wanghuajian620 <154659041@qq.com>
2017-09-14 15:07:25 +0800

    slice traning

---
+++ slice.js
@@ -0,0 +1,13 @@
+/**
+ * 2017-09-14    wanghujian
+ */
+
+var arr = ['chicken', 'duck','fish', 'pig', 'dog', 'lion', 'monkey'];
+console.log(`arr.slice(0, 4) >= [ ${arr.slice(0, 4)}]`);
+
+console.log(`arr.slice(1, 5)) >= [ ${arr.slice(1, 5)} ]`);
+
+console.log(arr.slice(5));
+console.log(`arr.slice(-1,5) >= [${arr.slice(-1,5)}]`);
+
+console.log(arr.slice(9));
