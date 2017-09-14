19b088020f2aa80a1a81631a1c6b7653d3604b17
wanghuajian620 <154659041@qq.com>
2017-09-14 16:09:42 +0800

    map training

--- array.js
+++ array.js
@@ -2,6 +2,7 @@
  * 2017-09-14   Wang Huajian
  */
 
+/*
 var arr = [10, 11];
 
 arr.unshift('pig', 'dog');
@@ -9,3 +10,10 @@ console.log(arr);
 
 arr.shift();
 console.log(arr);
+*/
+
+var arr = ['n', 'l', 'm'];
+
+console.log(arr.sort());
+
+va
---
+++ iterator.js
@@ -0,0 +1,9 @@
+/**
+ * 2017-09-19
+ */
+
+var arr = [1, 2, 3, 4, 5];
+
+console.log(arr.map(function(abc) {
+    return 3 * abc;
+}));
