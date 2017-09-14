b2e4b116e20e2f489a406d44da08e11bde0fe2d9
wanghuajian620 <154659041@qq.com>
2017-09-14 16:31:33 +0800

    filter  and reduce training

--- iterator.js
+++ iterator.js
@@ -4,6 +4,25 @@
 
 var arr = [1, 2, 3, 4, 5];
 
-console.log(arr.map(function(abc) {
+var func = function(abc) {
     return 3 * abc;
+};
+
+console.log(arr.map(func));
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8];
+var k = arr.filter(function(abc) {
+    return abc >= 3;
+});
+
+console.log(arr.filter(function(abc) {
+    return abc >= 3;
+}));
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];    
+var k = arr.reduce(function(prev, next) {
+    return prev + next;
+})
+console.log(arr.reduce(function(prev, next) {
+    return prev + next;
 }));
