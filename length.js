ac1451f85ef3abfdd474c3e7accf453d956e928c
wanghuajian620 <154659041@qq.com>
2017-09-14 14:02:55 +0800

    Verify array'slength

---
+++ length.js
@@ -0,0 +1,15 @@
+/**
+ * 2017-09-14    wanghuajian
+ */
+
+var arr = [1, 2, 3];
+console.log(arr.length);
+
+arr.length = 6;
+console.log(arr);
+
+arr.length = 2;
+console.log(arr);
+
+arr.length = 5;
+console.log(arr);
