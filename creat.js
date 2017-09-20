2489aab46056c2f0e8405ca6e5f6b8509b4a7455
wanghuajian620 <154659041@qq.com>
2017-09-20 11:40:29 +0800

    deep training

--- creat.js
+++ creat.js
@@ -2,7 +2,7 @@
  * 2017-09-19  Wang  Huajian
  */
 
-var person = {
+/*var person = {
     age : 2,
     name :  'Wang'
 };
@@ -24,10 +24,55 @@ console.log(a.luck());
 
 var b = Object.create(person,{});
 console.log(a.age, b.age);
+*/
 
 var a = [1, 2, 3, 4];
+//console.log(typeof a,typeof b);
+//console.log(a.__proto__);
+//console.log(b.__proto__);
+console.log(a.__proto__ === Object.prototype);
+console.log(a.__proto__.__proto__=== Object.prototype);
+console.log(a.__proto__=== Array.prototype);
+//console.log(`a.__proto__ === Function.__proto__=>${a.__proto__ === Function.__proto__}`);
+console.log('......................over......................');
+
+var x = 123;
+//console.log(typeof x);
+console.log( x.__proto__=== Object.prototype);
+console.log( x.__proto__.__proto__ === Object.prototype);
+console.log(x.__proto__ === Number.prototype);
+
+var y = true;
+//console.log(typeof y);
+console.log(y.__proto__ === Object.prototype);
+console.log(y.__proto__.__proto__ === Object.prototype);
+console.log(y.__proto__ === Boolean.prototype);
+
 var b = 'hello world';
-console.log(typeof a,typeof b);
-console.log(a.__proto__);
-console.log(b.__proto__);
-console.log(`a.__proto__===Function.__proto__=>${a.__proto__===Function.__proto__}`);
+console.log(b.__proto__ === Object.prototype);
+console.log(b.__proto__.__proto__ ===  Object.prototype);
+console.log(b.__proto__ === String.prototype);
+
+console.log(Function.prototype.constructor === Function);
+console.log(Array.prototype.constructor === Array);
+console.log(Number.prototype.constructor === Number);
+console.log(Boolean.prototype.constructor === Boolean);
+console.log(String.prototype.constructor === String);
+
+console.log(a.__proto__ === Function.__proto__);
+console.log(x.__proto__ === Function.__proto__);
+console.log(y.__proto__ === Function.__proto__);
+console.log(b.__proto__ === Function.__proto__);
+
+console.log(a.__proto__.__proto__ === Function.prototype.__proto__);
+console.log(a.__proto__.__proto__ === Object.prototype);
+console.log( x.__proto__=== Object.prototype);
+
+
+
+
+
+
+
+
+
